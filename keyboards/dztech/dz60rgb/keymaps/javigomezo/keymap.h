e5f6913a12441a8dfaae968eb397bb72c06891da
javigomezo <mail@javigomezo.com>
2024-03-19 21:11:24 +0100

    feat(dz60rgb): add initial keymaps

---
+++ keyboards/dztech/dz60rgb/keymaps/javigomezo/keymap.h
@@ -0,0 +1,7 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+#define PERMISSIVE_HOLD_PER_KEY true
